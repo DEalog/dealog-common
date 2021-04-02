6c80fa408c23352fbc6a31fea7d2e0b686c1e39c
ctietze <clemens.tietze@gmx.de>
2021-04-02 11:22:36 +0200

    Add exception for unsupported conversions

---
+++ src/main/java/de/dealog/common/converter/UnsupportedConversionException.java
@@ -0,0 +1,9 @@
+package de.dealog.common.converter;
+
+public class UnsupportedConversionException extends RuntimeException {
+
+    public UnsupportedConversionException(final String msg) {
+        super(msg);
+    }
+
+}
